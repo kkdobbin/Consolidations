f582707ca11e6756ab02722a17786bea59a7082d
Kristin Dobbin <kbdobbin@ucdavis.edu>
2022-06-23 17:19:13 -0700

    Made updates to master data, worked on final descriptives write up

--- Scripts/Data_compilation_descriptive_finaldata.R
+++ Scripts/Data_compilation_descriptive_finaldata.R
@@ -4,7 +4,8 @@
 library(tidyverse)
 
 #load data
-Master <- read_csv("Data/Master_copy_4.20.22.csv")
+Master <- read_csv("Data/Master_copy_6.22.22.csv")
+Master <- Master[-226,-c(36:151)]
 Consolidatedextra <- read_csv("Data/Consolidatedsystems_extradata_SWRCB.csv")
 Receivingextra <- read_csv("Data/Recievingsystem_extradata_EPA.csv")
 
@@ -18,8 +19,8 @@ Receivingextra$PWS.ID <- as.factor(Receivingextra$PWS.ID)
 #Add in extra data for consolidated systems (namely source)
 #reduce and rename variables to add into master with join, combine the state and federal primary source fields to reduce NAs
 Consolidatedextra$Consolidatedsystem_primary_source_combined <- ifelse(is.na(Consolidatedextra$D_FED_PRIM_SRC_CD), Consolidatedextra$D_ST_PRIM_SRC_CD, Consolidatedextra$D_FED_PRIM_SRC_CD) 
-
 Consolidatedextra_small <- Consolidatedextra %>% select(PWSID, Consolidatedsystem_primary_source_combined)
+Consolidatedextra_small <- Consolidatedextra_small[!duplicated(Consolidatedextra_small$PWSID), ] #remove duplicates
 Master <- left_join(Master, Consolidatedextra_small, by = c("SystemID" = "PWSID")) #still thirty or so that are NA because are still active systems (ie I didn't ask SWRCB for data for these systems) so try to add remaining ones using 2021 epa data
 
 Activecosnolidatedsystems <- Receivingextra %>% select('PWS ID', 'Primary Source Code')
@@ -28,20 +29,25 @@ Activecosnolidatedsystems <- rename(Activecosnolidatedsystems, EPA_2021_source =
 
 Master <- left_join(Master, Activecosnolidatedsystems, by = c("SystemID" = "PWSID"))
 
-Master$Consolidatedsystem_primary_source_combinedFINAL <- ifelse(is.na(Master$Consolidatedsystem_primary_source_combined), Master$EPA_2021_source, Master$Consolidatedsystem_primary_source_combined) #Down to only 6 missing sources
+Master$Consolidatedsystem_primary_source_combinedFINAL <- ifelse(is.na(Master$Consolidatedsystem_primary_source_combined), Master$EPA_2021_source, Master$Consolidatedsystem_primary_source_combined) 
+Master$Consolidatedsystem_primary_source_combinedFINAL[is.na(Master$Consolidatedsystem_primary_source_combinedFINAL)] = "GW"
+#Make five NAs groundwater (all private wells/state smalls plus kettleman city elementary school which I checked in EPA back records (there was a space in the PWSID for that one that I sent the board so didn't get it back I think))
 
 #add receiving extra data in (source and population)
-
-Receivingextra_small <- Receivingextra %>% select('PWS ID', 'Primary Source Code', 'Population Served Count')
+Receivingextra_small <- Receivingextra %>% select('PWS ID', 'Primary Source Code', 'Population Served Count', 'PWS Type')
 Receivingextra_small <- rename(Receivingextra_small, PWSID = `PWS ID`)
 Receivingextra_small <- rename(Receivingextra_small, Receiving_2021_source = 'Primary Source Code')
 Receivingextra_small <- rename(Receivingextra_small, Receiving_2021_pop = 'Population Served Count')
+Receivingextra_small <- rename(Receivingextra_small, Receiving_2021_systemtype = 'PWS Type')
 
 Master <- left_join(Master, Receivingextra_small, by = c( "Receiving_System_ID" =  "PWSID"))
 
 #Filter to remove cases tagged for removal
 Master$`Remove?` <- as.factor(Master$`Remove?`)
-Master <- Master %>% filter(`Remove?` == "No") #This removes 15 cases, note this in analysis write up and paper, maybe describe cases briefly
+Master <- Master %>% filter(`Remove?` == "No") #This removes 18 cases, note this in analysis write up and paper, describe cases and rationale briefly
+
+#Add in distance analysis data. NA means not analyzed (see notes distance_analysis_dataandnotes excel sheet)
+
 
 #write CSV
-write.csv(Master, file = "Outputs/CompiledApril20.csv", row.names = FALSE)
+write.csv(Master, file = "Outputs/Compiledfinaldata.csv", row.names = FALSE)
